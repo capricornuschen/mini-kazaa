10917002357f62fa04c02c201c50b522e1b6734c
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-18 16:52:07 +0000

    Added a first stub of callbacks.

---
+++ src/lpr/minikazaa/minikazaaclient/supernode/SupernodeRMIManager.java
@@ -0,0 +1,17 @@
+package lpr.minikazaa.minikazaaclient.supernode;
+
+/**
+ *
+ * @author Andrea Di Grazia, Massimiliano Giovine
+ * @date 18-set-2008
+ * @file SupernodeRMIManager.java
+ */
+public class SupernodeRMIManager implements Runnable {
+
+    public SupernodeRMIManager() {
+        
+    }
+
+    public void run() {
+    }
+}
