d30dda37ee80ae35ccaa9953600bfb3ddbdcc290
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-18 16:52:07 +0000

    Added a first stub of callbacks.

---
+++ trunk/src/lpr/minikazaa/minikazaaclient/supernode/SupernodeCallbacksInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lpr.minikazaa.minikazaaclient.supernode;
+
+/**
+ *
+ * @author Andrea Di Grazia, Massimiliano Giovine
+ * @date 18-set-2008
+ * @file SupernodeCallbacksInterface.java
+ */
+public interface SupernodeCallbacksInterface {
+
+}
