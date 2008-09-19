f42be065d94ac2efd510cdaa032ab2b82dee2d31
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-19 14:39:54 +0000

    Many files updated.

--- trunk/src/lpr/minikazaa/bootstrap/BootStrapCallbackImpl.java
+++ trunk/src/lpr/minikazaa/bootstrap/BootStrapCallbackImpl.java
@@ -7,6 +7,7 @@ package lpr.minikazaa.bootstrap;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import lpr.minikazaa.minikazaaclient.supernode.SupernodeCallbacksInterface;
 
@@ -19,22 +20,47 @@ import lpr.minikazaa.minikazaaclient.supernode.SupernodeCallbacksInterface;
 public class BootStrapCallbackImpl implements BootStrapCallbackInterface{
     
     List <SupernodeCallbacksInterface> client_list;
+    BootStrapGui bs_gui;
     
-    public BootStrapCallbackImpl(){
+    public BootStrapCallbackImpl(BootStrapGui gui){
         super();
-        client_list = new ArrayList <SupernodeCallbacksInterface>();
+        this.client_list = new ArrayList <SupernodeCallbacksInterface>();
+        this.bs_gui= gui;
     }
     
     public synchronized void registerForCallback(SupernodeCallbacksInterface client_callback) throws RemoteException{
-        
+        if(!client_list.contains(client_callback)){
+            client_list.add(client_callback);
+            System.out.println("Client successfuly added for callbacks.");
+            this.bs_gui.setWhatAppensLine(client_callback.toString()+" added.");
+            
+            //write log file
+        }
     }
     
     public synchronized void unregisterForCallback(SupernodeCallbacksInterface client_callback) throws RemoteException{
-        
+        if(client_list.remove(client_callback)){
+            System.out.println("Client unregistered.");
+            this.bs_gui.setWhatAppensLine(client_callback.toString()+" unregistered.");
+            //Write log file
+        }
+        else{
+            System.out.println("Unable to unregister client.");
+            this.bs_gui.setWhatAppensLine("Unable to unregister "+client_callback.toString()+".");
+            
+            //Write log file.
+        }
     }
     
     private synchronized void doCallbacks(){
+        System.out.println("Starting callbacks.");
         
+        Iterator i = client_list.iterator();
+        
+        while(i.hasNext()){
+            SupernodeCallbacksInterface client = (SupernodeCallbacksInterface) i.next();
+            //client.notifyMe( nodeinfo  );
+        }
         
     }
 }
