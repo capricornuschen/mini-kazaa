edbb8b6bc7a4d76fa3ede8f22a9c72b1f4cc4737
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-23 20:39:46 +0000

    SupernodeTCPListener added. Netbeans tryout.

---
+++ src/lpr/minikazaa/minikazaaclient/supernode/SupernodeTCPListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lpr.minikazaa.minikazaaclient.supernode;
+
+/**
+ *
+ * @author Andrea Di Grazia, Massimiliano Giovine
+ * @date 23-set-2008
+ * @file SupernodeTCPListener.java
+ */
+public class SupernodeTCPListener implements Runnable{
+    
+    public void run(){
+    //Server that liste all TCP requests.
+    }
+
+}
