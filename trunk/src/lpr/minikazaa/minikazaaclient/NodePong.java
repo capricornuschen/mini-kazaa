dd855fd6c9570bbfe0163383be76bb684248a99b
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-30 16:47:24 +0000

    NodePong added.

---
+++ trunk/src/lpr/minikazaa/minikazaaclient/NodePong.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lpr.minikazaa.minikazaaclient;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andrea Di Grazia, Massimiliano Giovine
+ * @date 30-set-2008
+ * @file NodePong.java
+ */
+public class NodePong implements Runnable {
+
+    NodeConfig my_conf;
+
+    public NodePong(NodeConfig conf) {
+        this.my_conf = conf;
+    }
+
+    public void run() {
+        int port = this.my_conf.getPort();
+        DatagramSocket pong_sock = null;
+        try {
+
+            pong_sock = new DatagramSocket(port);
+        } catch (SocketException ex) {
+            Logger.getLogger(NodePong.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while (true) {
+
+            byte[] packet = new byte[32];
+
+            DatagramPacket pack = new DatagramPacket(packet, packet.length);
+
+            try {
+
+                pong_sock.receive(pack);
+            } catch (IOException ex) {
+                Logger.getLogger(NodePong.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            packet = pack.getData();
+            DatagramSocket send_sock = null;
+            try {
+
+                send_sock = new DatagramSocket();
+            } catch (SocketException ex) {
+                Logger.getLogger(NodePong.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            byte [] send_byte = packet;
+            DatagramPacket send_pack = new DatagramPacket(send_byte,send_byte.length,pack.getAddress(),pack.getPort());
+            try {
+
+                send_sock.send(send_pack);
+            } catch (IOException ex) {
+                Logger.getLogger(NodePong.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+}
--- trunk/src/lpr/minikazaa/util/NetUtil.java
+++ trunk/src/lpr/minikazaa/util/NetUtil.java
@@ -8,6 +8,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 
 /**
  *
@@ -16,18 +17,21 @@ import java.util.Enumeration;
  * @file NetUtil.java
  */
 public class NetUtil {
-    
 
     public static InetAddress getAddress() throws SocketException {
+        InetAddress ipv6_addr = null;
+        InetAddress ipv4_addr = null;
+
         try {
+            System.out.println("Interfaccia controllata 0");
             NetworkInterface ni = NetworkInterface.getByName("eth0");
 
             Enumeration<InetAddress> ia_en = ni.getInetAddresses();
 
             //We use only ipv4 address.    
-            InetAddress ipv6_addr = ia_en.nextElement();
+            ipv6_addr = ia_en.nextElement();
             System.out.println(ipv6_addr.toString());
-            InetAddress ipv4_addr = ia_en.nextElement();
+            ipv4_addr = ia_en.nextElement();
             System.out.println(ipv4_addr.toString());
 
             return ipv4_addr;
@@ -35,36 +39,44 @@ public class NetUtil {
 
         } catch (NullPointerException ex0) {
             try {
+                System.out.println("Interfaccia controllata 1");
                 NetworkInterface ni = NetworkInterface.getByName("eth1");
 
                 Enumeration<InetAddress> ia_en = ni.getInetAddresses();
 
                 //We use only ipv4 address.    
-                InetAddress ipv6_addr = ia_en.nextElement();
+                ipv6_addr = ia_en.nextElement();
                 System.out.println(ipv6_addr.toString());
-                InetAddress ipv4_addr = ia_en.nextElement();
+                ipv4_addr = ia_en.nextElement();
                 System.out.println(ipv4_addr.toString());
 
                 return ipv4_addr;
             } catch (NullPointerException ex1) {
                 try {
+                    System.out.println("Interfaccia controllata 2");
                     NetworkInterface ni = NetworkInterface.getByName("eth2");
 
                     Enumeration<InetAddress> ia_en = ni.getInetAddresses();
 
                     //We use only ipv4 address.    
-                    InetAddress ipv6_addr = ia_en.nextElement();
+                    ipv6_addr = ia_en.nextElement();
                     System.out.println(ipv6_addr.toString());
-                    InetAddress ipv4_addr = ia_en.nextElement();
+                    ipv4_addr = ia_en.nextElement();
                     System.out.println(ipv4_addr.toString());
 
                     return ipv4_addr;
-                    
+
                 } catch (NullPointerException ex2) {
                     return null;
+                } catch (NoSuchElementException such1) {
+                    return ipv6_addr;
                 }
+            } catch (NoSuchElementException such2) {
+                return ipv6_addr;
             }
+        } catch (NoSuchElementException such3) {
+            return ipv6_addr;
         }
-        
+
     }
 }
