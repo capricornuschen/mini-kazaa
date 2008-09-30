eaff0d34dcb607150338f888c8ac7fe0465530f6
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-09-30 16:47:24 +0000

    NodePong added.

--- src/lpr/minikazaa/minikazaaclient/supernode/SupernodeGuiEngine.java
+++ src/lpr/minikazaa/minikazaaclient/supernode/SupernodeGuiEngine.java
@@ -2,15 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package lpr.minikazaa.minikazaaclient.supernode;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lpr.minikazaa.bootstrap.NodeInfo;
 import lpr.minikazaa.minikazaaclient.NodeConfig;
 import lpr.minikazaa.minikazaaclient.SupernodeList;
-
-
 
 /**
  *
@@ -18,27 +18,51 @@ import lpr.minikazaa.minikazaaclient.SupernodeList;
  * @date 29-set-2008
  * @file SupernodeGuiEngine.java
  */
-public class SupernodeGuiEngine implements Runnable{
-    
+public class SupernodeGuiEngine implements Runnable {
+
     SupernodeMainGui main_gui;
     NodeConfig my_conf;
     SupernodeList my_list;
-    
-    public SupernodeGuiEngine
-            (SupernodeMainGui gui,NodeConfig conf, SupernodeList list){
+
+    public SupernodeGuiEngine(SupernodeMainGui gui, NodeConfig conf, SupernodeList list) {
         this.main_gui = gui;
         this.my_conf = conf;
         this.my_list = list;
     }
-    
-    public void run(){
+
+    public void run() {
         //Do nothing untill list is unupdated.
-        while(my_list.getStatus() == false){}
-        
-        ArrayList <NodeInfo> sub_set_list = my_list.subSet(my_conf.getMaxConnection(), 100);
-        //Show subset of supernode
-        for(NodeInfo n : sub_set_list){
-            this.main_gui.addSuperNodeTableRow(n);
+        System.out.println("SupernodeGuiEngine init.");
+        while (true) {
+            while (my_list.getStatus() == false) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(SupernodeGuiEngine.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println("Ancora non modificata.");
+            }
+
+            ArrayList<NodeInfo> sub_set_list = my_list.subSet(my_conf.getMaxConnection(), 100);
+            
+            //Debug printlns
+            Iterator i = sub_set_list.iterator();
+            while(i.hasNext()){
+                System.out.println(((NodeInfo)i.next()).toString());
+            }
+            //Show subset of supernode
+            for (NodeInfo n : sub_set_list) {
+                this.main_gui.addSuperNodeTableRow(n);
+            }
+            System.out.println("Modifiche in corso.");
+            try {
+
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SupernodeGuiEngine.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+
+
     }
 }
