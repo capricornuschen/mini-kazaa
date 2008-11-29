a719080e656fdb6088796d8031808560eb2f4d6c
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2008-11-29 13:04:12 +0000

    Small update.

--- src/lpr/minikazaa/minikazaaclient/DownloadPartRequest.java
+++ src/lpr/minikazaa/minikazaaclient/DownloadPartRequest.java
@@ -6,6 +6,7 @@
 package lpr.minikazaa.minikazaaclient;
 
 import java.io.Serializable;
+import lpr.minikazaa.bootstrap.NodeInfo;
 
 /**
  *
@@ -16,16 +17,20 @@ import java.io.Serializable;
 public class DownloadPartRequest implements Serializable{
     private String file_request;
     private int part_n;
-    
+    private NodeInfo request_source;
+
     public DownloadPartRequest(
             String file,
-            int part_n){
+            int part_n,
+            NodeInfo ni){
     
         //Initialize the download request.
         this.file_request = file;
         this.part_n = part_n;
+        this.request_source = ni;
     }
     
     public String getFile(){return this.file_request;}
     public int getPart(){return this.part_n;}
+    public NodeInfo getSource(){return this.request_source;}
 }
