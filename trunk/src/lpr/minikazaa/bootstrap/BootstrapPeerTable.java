bfebe79bff487c9eb279c0b2712751e4712c4fa8
evocthurner <evocthurner@b2bb1bb1-b353-0410-b90e-f7582c0b44fc>
2009-01-06 21:36:58 +0000

    1# - Bootstrap Server program has been ended.

--- trunk/src/lpr/minikazaa/bootstrap/BootstrapPeerTable.java
+++ trunk/src/lpr/minikazaa/bootstrap/BootstrapPeerTable.java
@@ -2,13 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package lpr.minikazaa.bootstrap;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import lpr.minikazaa.minikazaaclient.SupernodeList;
 
 /**
  *
@@ -16,16 +17,47 @@ import javax.swing.table.DefaultTableModel;
  * @date 2-gen-2009
  * @file BootstrapPeerTable.java
  */
-public class BootstrapPeerTable extends JTable implements Observer{
+public class BootstrapPeerTable extends JTable implements Observer {
 
     DefaultTableModel my_dtm;
 
-    public BootstrapPeerTable(){
+    public BootstrapPeerTable() {
+        
+        this.my_dtm = new DefaultTableModel(
+                new Object[][]{},
+                new String[]{
+                    "Indirizzo"
+                }) {
+
+            boolean[] canEdit = new boolean[]{
+                false
+            };
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit[columnIndex];
+            }
+        };
+
+        this.setModel(this.my_dtm);
 
     }
 
     public void update(Observable o, Object arg) {
+        System.out.println("Update method called on bootstrap address table.");
+        if(o instanceof SupernodeList){
+            SupernodeList sn_list = (SupernodeList) o;
 
+            ArrayList<NodeInfo> node_list = sn_list.getList();
+
+            this.my_dtm.setRowCount(0);
+            for(NodeInfo node : node_list){
+                Object [] row = new Object[1];
+
+                row[0] = node.getId();
+
+                this.my_dtm.addRow(row);
+            }
+        }
     }
-
 }
